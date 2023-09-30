49eb8a89f23f92f4efb05e278922bdd40c02276f
Josh <143891382+joshwhite88@users.noreply.github.com>
2023-09-30 09:33:26 -0500

    added clickability on number guesser

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let teaCozyImg = document.getElementById('tea-cozy-pic');
 let teaCozyProj = document.getElementById('tea-cozy-proj');
+let numberGuesserImg = document.getElementById('number-guesser-pic');
+let numberGuesserProj = document.getElementById('number-guesser-proj');
 
 teaCozyProj.onclick = function() {
-    teaCozyImg.style.display = 'block';
+    if (teaCozyImg.style.display === 'block') {
+        teaCozyImg.style.display = 'none';
+    } else {
+        teaCozyImg.style.display = 'block';
+    }
+}
+
+numberGuesserProj.onclick = function() {
+    if (numberGuesserImg.style.display === 'block') {
+        numberGuesserImg.style.display = 'none';
+    } else {
+        numberGuesserImg.style.display = 'block';
+    }
 }
