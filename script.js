39f46b2ff8d88a1022e21a291295051c196ecf2b
Josh <143891382+joshwhite88@users.noreply.github.com>
2023-09-29 19:34:03 -0500

    added clickability of tea cozy project

---
+++ script.js
@@ -0,0 +1,6 @@
+let teaCozyImg = document.getElementById('tea-cozy-pic');
+let teaCozyProj = document.getElementById('tea-cozy-proj');
+
+teaCozyProj.onclick = function() {
+    teaCozyImg.style.display = 'block';
+}
